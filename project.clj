9c250f82c13805c68a29faf2e8f0e9e9bf6585f4
it0a <recursive404@gmail.com>
2015-05-31 18:33:41 -0400

    Adding create/drop migration table

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
 (defproject anemia "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "asynchronous schema mygrations"
+  :url "https://github.com/it0a/anemia"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :main anemia.core
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/java.jdbc "0.3.7"]
+                 [mysql/mysql-connector-java "5.1.6"]])
--- src/anemia/core.clj
+++ src/anemia/core.clj
@@ -1,6 +1,42 @@
-(ns anemia.core)
+(ns anemia.core
+  (:require [clojure.java.jdbc :as sql]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(let [db-host "0.0.0.0"
+      db-port 3306
+      db-name "schemex"]
+
+  (def db {:classname "com.mysql.jdbc.Driver"
+           :subprotocol "mysql"
+           :subname (str "//" db-host ":" db-port "/" db-name)
+           :user "root"
+           :password "sriq@"}))
+
+(defn- list-table-meta-data [db]
+  (-> (sql/get-connection db)
+      .getMetaData
+      (.getTables nil nil nil nil)
+      sql/metadata-result))
+
+(defn- list-table-names [db]
+  (map #(:table_name %) (list-table-meta-data db)))
+
+(defn- migration-table-exists? [db]
+  (not (empty? (filter #(= % "anemia_migrations") (list-table-names db)))))
+
+(defn- drop-migration-table [db]
+  (if (migration-table-exists? db)
+    (sql/db-do-commands
+     db
+     (sql/drop-table-ddl :anemia_migrations))))
+
+(defn create-migration-table [db]
+  (if-not (migration-table-exists? db)
+    (sql/db-do-commands
+     db
+     (sql/create-table-ddl :anemia_migrations [:name "VARCHAR(20)" "NOT NULL"])
+     "CREATE UNIQUE INDEX anemia_migrations_name_idx ON anemia_migrations (name)")))
+
+(defn -main [& args]
+  (println "unreleased"))
+
+
