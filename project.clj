2affb7aa4376d49a14775e9eed72023edade9d24
it0a <recursive404@gmail.com>
2015-06-01 22:14:09 -0400

    Updating project settings for lein-bin

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject seql "0.1.0-SNAPSHOT"
+(defproject seql "0.1.0"
   :description "schema migrations for the masses"
-  :url "https://github.com/it0a/anemia"
+  :url "https://github.com/it0a/seql"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main seql.core
@@ -8,4 +8,8 @@
                  [org.clojure/tools.cli "0.3.1"]
                  [org.clojure/java.jdbc "0.3.7"]
                  [mysql/mysql-connector-java "5.1.6"]
-                 [pandect "0.5.2"]])
+                 [pandect "0.5.2"]]
+  :plugins      [[lein-bin "0.3.4"]]
+  :aot [seql.core]
+  :omit-source true
+  :bin {:name "seql"})
