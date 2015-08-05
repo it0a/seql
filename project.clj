f4b913053e98b67dc511392488763354971c207e
it0a <recursive404@gmail.com>
2015-08-05 11:58:01 -0400

    0.1.6 => 0.1.7

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject seql "0.1.6"
+(defproject seql "0.1.7"
   :description "schema migrations for the masses"
   :url "https://github.com/it0a/seql"
   :license {:name "Eclipse Public License"
