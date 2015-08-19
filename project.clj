84bbe77265f4f5bfc6acf32bad37500f94d3cec7
it0a <recursive404@gmail.com>
2015-08-19 09:58:46 -0400

    Incrementing app version
    0.1.7 -> 0.1.8

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject seql "0.1.7"
+(defproject seql "0.1.8"
   :description "schema migrations for the masses"
   :url "https://github.com/it0a/seql"
   :license {:name "Eclipse Public License"
