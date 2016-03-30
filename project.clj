610fa46d2553e2fbc545b834a451f20807b51392
Jordan Gonzalez <jgonzalez@streamlinx.com>
2016-03-30 11:23:22 -0400

    Correcting to 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject seql "0.2.1"
+(defproject seql "0.2.0"
   :description "schema migrations for the masses"
   :url "https://github.com/it0a/seql"
   :license {:name "Eclipse Public License"
